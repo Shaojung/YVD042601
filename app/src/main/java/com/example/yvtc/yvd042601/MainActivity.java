91dc24b45f1e5c5098d68ab73117eaf2420b7335
Shaojung Lu <shaojung@gmail.com>
2017-04-26 09:20:48 +0800

    click on fragment a

--- app/src/main/java/com/example/yvtc/yvd042601/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvd042601/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.yvtc.yvd042601;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
